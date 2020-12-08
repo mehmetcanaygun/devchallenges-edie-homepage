daa1fb09a972f0d251aa84e55d4837ffd58de1ad
Can <mca199667@gmail.com>
2020-12-08 14:13:36 +0300

    Media Queries

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menuBtn = document.getElementById("menu-btn");
 const nav = document.querySelector(".navbar nav");
+const navLinks = document.querySelectorAll(".navbar nav ul li a");
 
 menuBtn.addEventListener("click", () => {
   if (!menuBtn.classList.contains("toggled")) {
@@ -9,4 +10,11 @@ menuBtn.addEventListener("click", () => {
     menuBtn.classList.remove("toggled");
     nav.classList.remove("toggled");
   }
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuBtn.classList.remove("toggled");
+    nav.classList.remove("toggled");
+  });
 });
